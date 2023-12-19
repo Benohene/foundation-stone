6b40fd54a04746dff9d64e9b742075337d631057
Benjamin Ohene Konadu <benohenekonadu@outlook.com>
2023-12-19 17:13:57 +0100

    added youtube video section with model and views

--- fsm_website/admin.py
+++ fsm_website/admin.py
@@ -1,3 +1,14 @@
+'''Admin configuration for the fsm_website app.'''
 from django.contrib import admin
+from .models import Video
 
-# Register your models here.
+
+class VideoAdmin(admin.ModelAdmin):
+    '''Admin View for Video'''
+    list_display = ('url',)
+    list_filter = ('url',)
+    search_fields = ('url',)
+    ordering = ('url',)
+
+
+admin.site.register(Video, VideoAdmin)
--- fsm_website/views.py
+++ fsm_website/views.py
@@ -1,6 +1,14 @@
+'''This file is used to render the html pages'''
 from django.shortcuts import render
+from .models import Video
 
-# Create your views here.
+
+# def index(request):
+#     '''This function is used to render the index.html page'''
+#     videos = Video.objects.all()
+#     return render(request, 'index.html', {'video': videos})
+
 def index(request):
-    return render(request, 'index.html')
-
+    # Assuming you want to get the latest video, you can use .first()
+    video = Video.objects.first()
+    return render(request, 'index.html', {'video': video})
